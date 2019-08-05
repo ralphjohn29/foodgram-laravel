eaae108443dccc0cf850cabf8c94f0eeeb7a57a6
ralphjohn29 <ralphjohn292@gmail.com>
2019-08-06 03:30:04 +0800

    Foodgram-laravel App

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+# Welcome to Foodgram
+
+This is for a food lovers social media platform.
+
+
+
+All the best,<br>
+Ralph,<br>
+
+@endcomponent
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/email', function (){
+    return new \App\Mail\NewUserWelcomeMail();
+});
+
+//Posts Routes
+
+Route::post('follow/{user}', 'FollowsController@store');
+
+
+Route::get('/', 'PostsController@index');
+Route::get('/p/create', 'PostsController@create');
+Route::get('/p/{post}', 'PostsController@show');
+Route::post('/p', 'PostsController@store');
+
+//Profiles Routes
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
